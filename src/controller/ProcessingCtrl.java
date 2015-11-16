9a6c9c9c1cc186567e7086d22ca143a9b00a901e
nsl42 <nslmdrhr@gmail.com>
2015-11-16 18:10:36 +0100

    ControllerMaj

--- src/controller/ProcessingCtrl.java
+++ src/controller/ProcessingCtrl.java
@@ -18,5 +18,26 @@ public class ProcessingCtrl extends Controller {
 	public ArrayList<Integer> getBibsNumber(UUID idPhoto) {
 		return null;
 	}
+	public UUID getPlidFromImgid(UUID imgId)
+	{
+		UUID ret = null;
+		for(UUID listID : ProcessingCtrl.loadedPhotoLists.keySet())
+		{
+			if(ProcessingCtrl.loadedPhotoLists.get(listID).contains(imgId))
+			{
+				ret = listID;
+				break;
+			}
+		}
+		return ret;
+	}
+	public String imgProcessing(UUID imgID)
+	{
+		return ProcessingCtrl.loadedPhotoLists.get(getPlidFromImgid(imgID)).process();
+	}
+	public String listProcessing(UUID photoListID)
+	{
+		return ProcessingCtrl.loadedPhotoLists.get(photoListID).process();
+	}
 
 }
