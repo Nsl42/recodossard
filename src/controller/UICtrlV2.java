f450a88eb9d186053f80e58f0199c8b84c4f9b9b
nsl42 <nslmdrhr@gmail.com>
2015-12-03 19:09:30 +0100

    Controller mini fix

--- src/controller/UICtrlV2.java
+++ src/controller/UICtrlV2.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import java.awt.Image;
@@ -13,28 +8,44 @@ import java.util.HashMap;
 import java.util.UUID;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
-import javax.swing.JScrollPane;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 import model.ImgModel;
 import model.PhotoList;
 import review.DataPanelV2;
 import review.GUIV2;
 
 /**
- *
- * @author nsl
+ * Manages and refreshes the view
+ * @author Anas Alaoui M'Darhri
  */
 public class UICtrlV2 extends Controller{
+
 	public static GUIV2 gui;
-	static private ImgModel selected;
 	public static HashMap<UUID, ArrayList<JLabel>> labels = new HashMap<>();
-
+	static private ImgModel selected;
 	static private PhotoList current;
+	static private DefaultMutableTreeNode rootNode = new DefaultMutableTreeNode("Gallery list :");
+	static private DefaultTreeModel treeModel =
+		new DefaultTreeModel(rootNode);
 
+	// Getters and Setters
+
+	/** 
+	 * @author Anas Alaoui M'Darhri
+	 * @return currently selected object
+	 */
 	public static ImgModel getSelected() {
 		return selected;
 	}
 	
+	/**
+	 * @author Anas ALaoui M'Darhri
+	 * @param selected id of the newly selected photo
+	 */
 	public static void setSelected(UUID selected) {
 		if(selected == null)
 			UICtrlV2.selected = null;
@@ -45,21 +56,11 @@ public class UICtrlV2 extends Controller{
 					UICtrlV2.selected = im;
 	}
 	
-	public static void closeNoPl()
-	{
-		if(!UICtrlV2.loadedPhotoLists.isEmpty())
-		{
-		gui.remove(gui.getNplp());
-		gui.getNplp().setVisible(false);
-		System.out.println("NpLp  " + gui.getMp().isVisible());
-		System.out.println("Mp  " + gui.getMp().isVisible());
-		gui.getMp().setVisible(true);
-		System.out.println("closeNoPl");
-		gui.validate();
-		gui.repaint();
-		}
-	}
-	
+	/**
+	 * @author Anas Alaoui M'Darhri
+	 * @param toCurrent the new PhotoList you want to display on the 
+	 * main panel
+	 */
 	static public void setCurrent(UUID toCurrent) {
 		UICtrlV2.current = ProcessingCtrl.loadedPhotoLists.get(toCurrent);
 		System.out.println("curr " + current.toString());
@@ -68,17 +69,63 @@ public class UICtrlV2 extends Controller{
 		UICtrlV2.gui.repaint();
 	}
 	
+	/**
+	 * @author Anas Alaoui M'Darhri
+	 * @return the currently displayed photolist
+	 */
+
 	static public PhotoList getCurrent() {
 		return current;
 	}
+
+	// VIEW REFRESHING METHODS
 	
+	/**
+	 * Refreshes the view when the first photolist has been added
+	 * @author Anas Alaoui M'Darhri
+	 */
+	public static void closeNoPl()
+	{
+		if(!UICtrlV2.loadedPhotoLists.isEmpty())
+		{
+		gui.remove(gui.getNplp());
+		gui.getNplp().setVisible(false);
+		gui.getMp().setVisible(true);
+		System.out.println("NpLp  " + gui.getMp().isVisible());
+		System.out.println("Mp  " + gui.getMp().isVisible());
+		System.out.println("closeNoPl");
+		gui.validate();
+		gui.repaint();
+		}
+	}
+	
+	/**
+	 * Refreshes the Img-related data on the datapanel
+	 * @author Anas Alaoui M'Darhri
+	 * @return the updated and refreshed DataPanel
+	 */
 	static public DataPanelV2 refreshImgDP(){
 		return UICtrlV2.gui.getMp().getDp().refreshImg();
 	}
+
+	/**
+	 * Refreshes the PhotoList-related data on the datapanel
+	 * @author Anas Alaoui M'Darhri
+	 * @return the updated and refreshed DataPanel
+	 */
 	static public DataPanelV2 refreshDataDP(){
 		return UICtrlV2.gui.getMp().getDp().refreshData();
 	}
 	
+	
+	// OBJECT INSTANCIATING METHODS
+	
+	/**
+	 * Given a photoList, builds for each picture a label to display  in the 
+	 * main view
+	 * @author Anas Alaoui M'Darhri
+	 * @param pl a existing PhotoList
+	 */
 	static public void buildLabels(PhotoList pl) {
 		ArrayList<JLabel> ret = new ArrayList<>();
 		for(final ImgModel im : pl.getPhotolist()) {
@@ -107,5 +154,23 @@ public class UICtrlV2 extends Controller{
 
 		labels.put(pl.getId(), ret);
 	}
+	// GENERIC LISTENERS
+	public static void addGalleryAction(JButton btn){
+		JFileChooser jfc = new JFileChooser();
+		jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		jfc.setMultiSelectionEnabled(false);
+		if(jfc.showOpenDialog(btn) == JFileChooser.APPROVE_OPTION){
+			if(UICtrlV2.loadedPhotoLists.isEmpty())
+				UICtrlV2.gui.getMp().getPll().removeAll();
+			UUID uid = ProcessingCtrl.
+				acknowledge(jfc.getSelectedFile());
+			UICtrlV2.closeNoPl();
+			UICtrlV2.buildLabels(UICtrlV2.loadedPhotoLists.get(uid));
+			UICtrlV2.setCurrent(uid);
+			UICtrlV2.gui.getMp().getPll().add(UICtrlV2.loadedPhotoLists.get(uid));
+			UICtrlV2.gui.repaint();
+			System.out.println("HELLO REPAINT");
+		}
 	
+	}
 }
